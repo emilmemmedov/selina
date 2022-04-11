8a29641794f0a93e7d14e4d35fdffa1b8e255e5d
emilmemmedov <emill.memmedovv@gmail.com>
2022-04-11 20:51:05 +0400

    project structure initialization

--- application/src/app.module.ts
+++ application/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './infrastructure/config/ormconfig';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeormConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- application/src/infrastructure/config/ormconfig.ts
+++ application/src/infrastructure/config/ormconfig.ts
@@ -1,16 +1,17 @@
 import { config } from 'dotenv';
 import { get } from 'env-var';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 config();
 
-export const typeormConfig = {
+export const typeormConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: get('DB_HOST').required().asString(),
   port: get('DB_PORT').required().asIntPositive(),
   username: get('DB_USERNAME').required().asString(),
   password: get('DB_PASSWORD').required().asString(),
-  database: get('DB_NAME').required().asString(),
-  entities: [],
+  database: get('DB_DATABASE').required().asString(),
+  entities: ['dist/**/*.entity{.ts,.js}'],
   autoLoadEntities: true,
   connectTimeoutMS: 2000,
   logging: ['error', 'migration', 'schema'],
